855c9a783aecb9efab98df827080b4f09cf1567c
Szymon Tyburek <szymontyburek7@gmail.com>
2023-12-04 15:25:44 -0700

    renamed highlight_on_search function, since it does more than just that

--- grid-plugin.js
+++ grid-plugin.js
@@ -292,7 +292,7 @@
         default_sf = this.value;
         //if there is a search currently in the textbox, need to call highight_on_search function for an accurate response/filter
         if (search_mode.get()) {
-          highlight_on_search();
+          grid_args();
         }
       });
 
@@ -333,11 +333,45 @@
         } else {
           search_mode.set(false);
         }
-        highlight_on_search();
+        grid_args();
       });
     };
 
-    const highlight_on_search = () => {
+    const grid_args = () => {
+      //     function sortTable() {
+      //       var table, rows, switching, i, x, y, shouldSwitch;
+      //       table = document.getElementById("myTable");
+      //       switching = true;
+      //       /* Make a loop that will continue until
+      // no switching has been done: */
+      //       while (switching) {
+      //         // Start by saying: no switching is done:
+      //         switching = false;
+      //         rows = table.rows;
+      //         /* Loop through all table rows (except the
+      //   first, which contains table headers): */
+      //         for (i = 1; i < rows.length - 1; i++) {
+      //           // Start by saying there should be no switching:
+      //           shouldSwitch = false;
+      //           /* Get the two elements you want to compare,
+      //     one from current row and one from the next: */
+      //           x = rows[i].getElementsByTagName("TD")[0];
+      //           y = rows[i + 1].getElementsByTagName("TD")[0];
+      //           // Check if the two rows should switch place:
+      //           if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+      //             // If so, mark as a switch and break the loop:
+      //             shouldSwitch = true;
+      //             break;
+      //           }
+      //         }
+      //         if (shouldSwitch) {
+      //           /* If a switch has been marked, make the switch
+      //     and mark that a switch has been done: */
+      //           rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      //           switching = true;
+      //         }
+      //       }
+      //     }
       conf.data_adapter.load(function (data) {
         if (typeof data !== "undefined") {
           const typed_text = search_bar.value.toLowerCase();
@@ -1296,7 +1330,7 @@
       curr_page = new_page;
       first_entry_index = (new_page - 1) * conf.rtd;
       pag_tb.value = curr_page;
-      highlight_on_search();
+      grid_args();
     };
 
     this.api = {
